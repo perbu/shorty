4e172cb3fd2edb7e505f81df36d6759e75e9fb40
Per Buer <per.buer@gmail.com>
2019-07-05 14:04:33 +0200

    Move code into a class and make a runner

--- app.js
+++ app.js
@@ -1,29 +1,7 @@
-
 "use strict";
 
-//app.js
-const express = require('express');
-const bodyParser = require('body-parser');
-const url = require('./routes/url.route'); 
-const app = express();
+const Shorty = require('./shorty');
 
+const server = new Shorty(3000);
 
-// Set up mongoose connection
-const mongoose = require('mongoose');
-let dev_db_url = 'mongodb://localhost/shorty';
-let mongoDB = process.env.MONGODB_URI || dev_db_url;
-mongoose.connect(mongoDB, { useNewUrlParser: true });
-mongoose.Promise = global.Promise;
-let db = mongoose.connection;
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
-
-
-app.use('/', url);
-let port = 3000;
-
-app.listen(port, () => {
-    console.log('Server is up and running on port numner ' + port);
-});
+server.startup();
